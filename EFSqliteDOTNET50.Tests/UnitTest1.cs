0d71af43283db4820bcc0bbb9ca367a144ad0adb
Brice Grenard <brice.grenard@gmail.com>
2021-05-11 13:57:34 +0100

    Just a quick test project to check how to execute code testing with bitbucket deployment

---
+++ EFSqliteDOTNET50.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+using NUnit.Framework;
+
+namespace EFSqliteDOTNET50.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
